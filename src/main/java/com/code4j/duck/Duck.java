562d45e10cd95370d5c3bce32e2f6ffee09c4199
code4j-chen <code4j.chen@gmail.com>
2015-01-12 17:01:41 +0800

    第一次设计

---
+++ src/main/java/com/code4j/duck/Duck.java
@@ -0,0 +1,18 @@
+package com.code4j.duck;
+
+/**
+ * Created by code4j on 2015/1/12.
+ */
+public class Duck {
+    protected void quack(){
+        System.out.println("quack");
+    }
+
+    protected void swim() {
+        System.out.println("swim");
+    }
+
+    protected void display() {
+        System.out.println("display");
+    }
+}
---
+++ src/main/java/com/code4j/duck/MallardDuck.java
@@ -0,0 +1,11 @@
+package com.code4j.duck;
+
+/**
+ * Created by code4j on 2015/1/12.
+ */
+public class MallardDuck extends Duck {
+    @Override
+    protected void display() {
+        System.out.println("green");
+    }
+}
