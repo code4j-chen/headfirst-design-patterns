454dadf28ed09266b22a1a2cb33281368dfc5272
code4j-chen <code4j.chen@gmail.com>
2015-01-13 19:42:01 +0800

    使用装饰者模式，自定义的IO

---
+++ src/main/java/com/code4j/starbuzz/io/InputTest.java
@@ -0,0 +1,20 @@
+package com.code4j.starbuzz.io;
+
+import java.io.*;
+
+/**
+ * Created by code4j on 2015/1/13.
+ */
+public class InputTest {
+    public static void main(String[] args) {
+        int c;
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char)c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
