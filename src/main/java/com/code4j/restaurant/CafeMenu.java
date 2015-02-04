1f4ed3d4aa6eab6fa6271ecab9e2c81f74a444d9
code4j-chen <code4j.chen@gmail.com>
2015-02-04 18:16:47 +0800

    迭代

--- src/main/java/com/code4j/restaurant/CafeMenu.java
+++ src/main/java/com/code4j/restaurant/CafeMenu.java
@@ -1,15 +1,17 @@
 package com.code4j.restaurant;
 
-import java.util.Hashtable;
+import java.util.*;
+import java.util.Iterator;
 
 /**
  * Created by code4j on 2015/1/27.
  */
-public class CafeMenu {
+public class CafeMenu implements Menu{
     Hashtable<String, MenuItem> menuItems = new Hashtable();
 
     public CafeMenu() {
         addItem("Veggie Burger and Air Fies", "Veggie burger on a whole wheat bun, lettuce", true, 3.99);
+        addItem("Soup of the day", "Veggie burger on a whole wheat bun, lettuce", true, 3.99);
     }
 
     private void addItem(String name, String description, boolean vegetarian, double price) {
@@ -17,7 +19,12 @@ public class CafeMenu {
         menuItems.put(menuItem.getName(), menuItem);
     }
 
-    public Hashtable<String, MenuItem> getItems() {
+    /*public Hashtable<String, MenuItem> getItems() {
         return menuItems;
+    }*/
+
+    @Override
+    public Iterator createIterator() {
+        return menuItems.values().iterator();
     }
 }
--- src/main/java/com/code4j/restaurant/Waitress.java
+++ src/main/java/com/code4j/restaurant/Waitress.java
@@ -7,19 +7,24 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator<MenuItem> iterator = pancakeHouseMenu.createIterator();
         Iterator<MenuItem> iterator1 = dinerMenu.createIterator();
+        Iterator iterator2 = cafeMenu.createIterator();
         System.out.println("MENU\n----\nBREAKFAST");
         printMenu(iterator);
         System.out.println("\nLUNCH");
         printMenu(iterator1);
+        System.out.println("\nDINNER");
+        printMenu(iterator2);
     }
 
     private void printMenu(Iterator<MenuItem> iterator) {
